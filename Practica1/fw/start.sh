8616775683498e56958655c605a4df33e9f75be2
AlvaroRodriguez9 <arodrc27@estudiantes.unileon.es>
2022-10-31 19:31:47 +0100

    Arreglada forward int-ext

--- Practica1/fw/start.sh
+++ Practica1/fw/start.sh
@@ -2,6 +2,7 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT 
 
 iptables -A INPUT -i eth0 -p tcp -m multiport --dports 22,80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp -m multiport --dports 22,80,443 -m state --state ESTABLISHED -j ACCEPT
@@ -12,6 +13,20 @@ iptables -A OUTPUT -o eth2 -p tcp -m multiport --dports 22,80,443 -m state --sta
 
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
+
+
+
+iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A FORWARD -i eth2 -o eth1 -s 10.5.2.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth2 -d 10.5.2.0/24 -p icmp --icmp-type echo-reply -j ACCEPT
+
+iptables -A FORWARD -i eth2 -o eth1 -s 10.5.2.0/24 -p tcp -m tcp --dport 22 -j ACCEPT 
+iptables -A FORWARD -i eth1 -o eth2 -d 10.5.2.0/24 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
+
 
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
