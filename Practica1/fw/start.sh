1b678b3039b3e31370e26146ea5823f47b4802d4
AlvaroRodriguez9 <arodrc27@estudiantes.unileon.es>
2022-11-01 10:52:28 +0100

    Cambios menores

--- Practica1/fw/start.sh
+++ Practica1/fw/start.sh
@@ -2,14 +2,8 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT 
-
-iptables -A INPUT -i eth0 -p tcp -m multiport --dports 22,80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp -m multiport --dports 22,80,443 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth1 -p tcp -m multiport --dports 22,80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth1 -p tcp -m multiport --dports 22,80,443 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth2 -p tcp -m multiport --dports 22,80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth2 -p tcp -m multiport --dports 22,80,443 -m state --state ESTABLISHED -j ACCEPT
+ 
+iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
 
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
