7f28f76132a8838a6f5af4b7e41d07d5ffdb6377
AlvaroRodriguez9 <arodrc27@estudiantes.unileon.es>
2022-11-11 21:07:37 +0100

    V final comentada

--- Practica1/dmz/start.sh
+++ Practica1/dmz/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Script que se ejecuta al hacer exec. Contiene la nueva dirección del gateway. También reinicia apache2
+# Autor: Álvaro Rodríguez Carpintero
 route del default gw 10.5.1.254
 route add default gw 10.5.1.1
 
